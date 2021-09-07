030a9245ad2da43455da4e453c7c57ac0ba59c18
Emrullah Taştan <emrullah69@gmail.com>
2021-09-07 07:44:23 +0300

    change responsive config

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,17 @@
         <div class="nav__left-side">
           <router-link to="/" class="flex align-item-center">
             <svg
-              class="home-icon"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="512"
-              height="512"
-              viewBox="0 0 512 512"
+                class="home-icon"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="512"
+                height="512"
+                viewBox="0 0 512 512"
             >
               <title></title>
               <g id="icomoon-ignore"></g>
               <path
-                d="M512 295.222l-256-198.713-256 198.714v-81.019l256-198.713 256 198.714zM448 288v192h-128v-128h-128v128h-128v-192l192-144z"
+                  d="M512 295.222l-256-198.713-256 198.714v-81.019l256-198.713 256 198.714zM448 288v192h-128v-128h-128v128h-128v-192l192-144z"
               ></path>
             </svg>
             <span class="ml-10 mt-5"> Sample Project </span>
@@ -23,60 +23,62 @@
         </div>
         <div class="nav__right-side" :class="openMobileMenu ? 'open' : null">
           <router-link to="/about" class="nav__right-side-item"
-            >{{ $t("menu.about") }}
+          >{{ $t("menu.about") }}
           </router-link>
           <router-link
-            v-if="userInfo['email']"
-            to="/login"
-            class="nav__right-side-item"
+              v-if="userInfo['email']"
+              to="/login"
+              class="nav__right-side-item"
           >
             <span @click="logout">
               {{ $t("menu.logout") }}
             </span>
           </router-link>
           <router-link
-            v-if="!userInfo['email']"
-            to="/login"
-            class="nav__right-side-item"
-            >{{ $t("menu.login") }}
+              v-if="!userInfo['email']"
+              to="/login"
+              class="nav__right-side-item"
+          >{{ $t("menu.login") }}
           </router-link>
           <div style="display: inline-flex" class="nav__right-side-item">
             <div
-              v-if="visibleLangMenuItem('tr')"
-              style="cursor: pointer"
-              @click="changeLang('tr')"
+                v-if="visibleLangMenuItem('tr')"
+                style="cursor: pointer"
+                @click="changeLang('tr')"
             >
               Turkish
             </div>
             <div
-              v-if="visibleLangMenuItem('en')"
-              style="cursor: pointer"
-              @click="changeLang('en')"
+                v-if="visibleLangMenuItem('en')"
+                style="cursor: pointer"
+                @click="changeLang('en')"
             >
               English
             </div>
           </div>
-          <div v-if="userInfo['email']" style="display: inline">
-            | {{ $t("hello") + " " + userInfo["name"] }}
+          <div class="user-info" v-if="userInfo['email']" style="display: inline">
+            {{ $t("hello") + " " + userInfo["name"] }}
           </div>
         </div>
         <div class="nav__mobile-menu-icon" @click="openMobileMenuFunc">
           <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
           >
             <title>menu</title>
             <g id="icomoon-ignore"></g>
             <path
-              d="M32 96h448v96h-448zM32 224h448v96h-448zM32 352h448v96h-448z"
+                d="M32 96h448v96h-448zM32 224h448v96h-448zM32 352h448v96h-448z"
             ></path>
           </svg>
         </div>
       </div>
     </div>
-    <div style="padding-top: 50px">
-      <router-view />
+    <div class="flex">
+      <div class="container" style="padding-top: 20px">
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
@@ -89,19 +91,19 @@ export default {
   data() {
     return {
       userInfo: this.getUserInfo(),
-      openMobileMenu: false,
+      openMobileMenu: false
     };
   },
   watch: {
     $route() {
       this.openMobileMenu = false;
-    },
+    }
   },
   methods: {
     ...mapGetters({ getUserInfo: "getUserInfo" }),
     ...mapActions({
       setLangKey: "setLanguage",
-      clearUserInfo: "clearUserInfo",
+      clearUserInfo: "clearUserInfo"
     }),
     changeLang(langKey) {
       this.setLangKey({ langKey });
@@ -115,8 +117,8 @@ export default {
     },
     openMobileMenuFunc() {
       this.openMobileMenu = !this.openMobileMenu;
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="scss">
@@ -126,7 +128,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,93 @@
 <template>
   <div class="home">
-    <h4>{{$t('home.title')}}</h4>
+    <h4 class="page-title">{{ $t("home.title") }}</h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Magna ac placerat
+      vestibulum lectus mauris ultrices. Habitasse platea dictumst quisque
+      sagittis purus sit. Ultrices gravida dictum fusce ut. Habitant morbi
+      tristique senectus et netus et malesuada fames ac. Duis at consectetur
+      lorem donec massa. Quis commodo odio aenean sed adipiscing diam. Phasellus
+      faucibus scelerisque eleifend donec pretium vulputate sapien nec. Odio
+      facilisis mauris sit amet massa vitae. Adipiscing commodo elit at
+      imperdiet dui accumsan sit amet. At consectetur lorem donec massa sapien
+      faucibus et molestie ac. Dignissim suspendisse in est ante in nibh. Eget
+      est lorem ipsum dolor sit amet. Et tortor consequat id porta nibh
+      venenatis. Pharetra massa massa ultricies mi quis hendrerit dolor. Diam
+      sollicitudin tempor id eu nisl nunc mi ipsum.
+    </p>
+
+    <p>
+      Libero volutpat sed cras ornare arcu dui. Posuere ac ut consequat semper.
+      Gravida cum sociis natoque penatibus et magnis dis parturient. Aliquam
+      ultrices sagittis orci a scelerisque purus semper. Id porta nibh venenatis
+      cras sed felis eget velit. Tortor id aliquet lectus proin nibh nisl.
+      Cursus metus aliquam eleifend mi in nulla posuere. Ac tortor dignissim
+      convallis aenean et tortor at risus viverra. Sed velit dignissim sodales
+      ut eu sem integer. Consectetur a erat nam at lectus urna duis convallis.
+      Eget nulla facilisi etiam dignissim. Pellentesque elit ullamcorper
+      dignissim cras tincidunt. Gravida rutrum quisque non tellus orci.
+      Scelerisque eleifend donec pretium vulputate sapien nec. Morbi blandit
+      cursus risus at. Magna sit amet purus gravida quis blandit turpis. Viverra
+      maecenas accumsan lacus vel facilisis volutpat est velit egestas.
+    </p>
+
+    <p>
+      Ante metus dictum at tempor commodo ullamcorper a. Consectetur adipiscing
+      elit ut aliquam purus. Nam at lectus urna duis convallis convallis tellus
+      id interdum. Risus commodo viverra maecenas accumsan lacus. Duis ut diam
+      quam nulla porttitor. Fermentum dui faucibus in ornare. Eu scelerisque
+      felis imperdiet proin. Adipiscing diam donec adipiscing tristique. Iaculis
+      eu non diam phasellus vestibulum lorem sed risus ultricies. Elementum
+      facilisis leo vel fringilla est. Id nibh tortor id aliquet lectus proin
+      nibh nisl condimentum. Gravida arcu ac tortor dignissim convallis. Turpis
+      tincidunt id aliquet risus feugiat in. Vitae tortor condimentum lacinia
+      quis vel eros donec ac. Convallis tellus id interdum velit laoreet id.
+      Faucibus interdum posuere lorem ipsum dolor sit. Lacinia quis vel eros
+      donec ac odio. At consectetur lorem donec massa sapien faucibus. Elit eget
+      gravida cum sociis natoque penatibus et magnis dis. Elit sed vulputate mi
+      sit amet mauris commodo quis imperdiet.
+    </p>
+
+    <p>
+      Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna.
+      Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum.
+      Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies.
+      Aliquam purus sit amet luctus venenatis lectus magna fringilla urna.
+      Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Pellentesque
+      id nibh tortor id aliquet lectus proin nibh. Id ornare arcu odio ut. Nibh
+      mauris cursus mattis molestie. Sagittis nisl rhoncus mattis rhoncus urna
+      neque viverra. Arcu dictum varius duis at consectetur lorem donec massa.
+      Malesuada fames ac turpis egestas integer eget. Augue eget arcu dictum
+      varius duis at consectetur lorem. Nibh ipsum consequat nisl vel pretium
+      lectus quam id. Cursus vitae congue mauris rhoncus. Eleifend mi in nulla
+      posuere sollicitudin. Imperdiet massa tincidunt nunc pulvinar sapien et
+      ligula ullamcorper. Adipiscing tristique risus nec feugiat in fermentum
+      posuere urna nec. Elit ut aliquam purus sit amet luctus venenatis. Nulla
+      facilisi morbi tempus iaculis. Donec enim diam vulputate ut pharetra sit
+      amet aliquam id.
+    </p>
+
+    <p>
+      Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant
+      morbi. In vitae turpis massa sed elementum. Feugiat scelerisque varius
+      morbi enim nunc faucibus. Faucibus nisl tincidunt eget nullam non nisi
+      est. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus.
+      Aenean euismod elementum nisi quis eleifend quam adipiscing. Dictum sit
+      amet justo donec enim. Turpis nunc eget lorem dolor. Convallis a cras
+      semper auctor neque vitae tempus quam. Congue quisque egestas diam in arcu
+      cursus. Congue nisi vitae suscipit tellus mauris a diam. Convallis
+      convallis tellus id interdum velit laoreet id donec. Massa tincidunt dui
+      ut ornare lectus sit amet. Cras semper auctor neque vitae tempus quam
+      pellentesque. Mauris a diam maecenas sed enim ut sem viverra. Pretium
+      lectus quam id leo in vitae. Vel orci porta non pulvinar neque laoreet
+      suspendisse interdum. Amet consectetur adipiscing elit duis tristique.
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
 };
 </script>
